ce696cc99edd73402e50eeca91beb4fa4b12d84f
Eliot Jones <elioty@hotmail.co.uk>
2015-02-10 19:48:21 +0000

    Added first draft of radio button collection

--- EliotJones.MvcHelpers/Controllers/HomeController.cs
+++ EliotJones.MvcHelpers/Controllers/HomeController.cs
@@ -1,12 +1,40 @@
 ﻿namespace EliotJones.MvcHelpers.Controllers
 {
+    using EliotJones.MvcHelpers.ViewModels;
+    using System.Collections.Generic;
     using System.Web.Mvc;
 
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            PepperStuff capsicums = new PepperStuff
+            {
+                Quantity = 0,
+                Peppers = new StringIntCollection
+                {
+                    PossibleValues = new List<KeyValuePair<string, int>>
+                    {
+                        new KeyValuePair<string, int>("red", 1),
+                        new KeyValuePair<string, int>("green", 2),
+                        new KeyValuePair<string, int>("yellow", 3)
+                    }
+                }
+            };
+
+            return View(capsicums);
+        }
+
+        [HttpPost]
+        public ActionResult Index(PepperStuff capsicums)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(capsicums);
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ EliotJones.MvcHelpers/ViewModels/PepperType.cs
@@ -0,0 +1,9 @@
+﻿namespace EliotJones.MvcHelpers.ViewModels
+{
+    public class PepperStuff
+    {
+        public RadioButtonCollection<string, int> Peppers { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
