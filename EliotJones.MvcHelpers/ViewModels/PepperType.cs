2cb394a1d0a4aac7554d3045cf7a7b01789f5fbe
Eliot Jones <elioty@hotmail.co.uk>
2015-02-10 20:08:17 +0000

    changes to string int collection

--- EliotJones.MvcHelpers/ViewModels/PepperType.cs
+++ EliotJones.MvcHelpers/ViewModels/PepperType.cs
@@ -2,7 +2,7 @@
 {
     public class PepperStuff
     {
-        public RadioButtonCollection<string, int> Peppers { get; set; }
+        public RadioButtonCollection<string, int, int?> Peppers { get; set; }
 
         public int Quantity { get; set; }
     }
