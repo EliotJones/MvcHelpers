ff1927b6a5cfce6b75489027a53d64b515847db4
Eliot Jones <elioty@hotmail.co.uk>
2015-02-11 19:08:07 +0000

    Added missing files

---
+++ EliotJones.MvcHelpers/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EliotJones.MvcHelpers.Startup))]
+namespace EliotJones.MvcHelpers
+{
+    using System.Web.Mvc;
+    using System.Web.Optimization;
+    using System.Web.Routing;
+
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
---
+++ EliotJones.MvcHelpers/ViewModels/RadioButtonCollection.cs
@@ -0,0 +1,31 @@
+﻿namespace EliotJones.MvcHelpers.ViewModels
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class RadioButtonCollection<TKey, TValue, TValueNullable>
+    {
+        public virtual IList<RadioButtonPair<TKey, TValue>> PossibleValues { get; set; }
+
+        public virtual TValueNullable SelectedValue { get; set; }
+
+        public TKey SelectedKey { get; set; }
+    }
+
+    public class RadioButtonPair<TKey, TValue>
+    {
+        public TKey Key { get; set; }
+
+        public TValue Value { get; set; }
+    }
+
+    public class StringIntCollection : RadioButtonCollection<string, int, int?>
+    {
+    }
+
+    public class StringIntCollectionRequired : StringIntCollection
+    {
+        [Required]
+        public override int? SelectedValue { get; set; }
+    }
+}
