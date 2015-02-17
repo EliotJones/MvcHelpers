9473bc82453134ecb626de1ff26fb8067e1d6470
Eliot Jones <elioty@hotmail.co.uk>
2015-02-17 21:08:42 +0000

    Added xml comments.

--- EliotJones.RadioButtons/ViewModels/StringIntRadioButtons.cs
+++ EliotJones.RadioButtons/ViewModels/StringIntRadioButtons.cs
@@ -7,11 +7,18 @@
     /// </summary>
     public class StringIntRadioButtons : KeyValueRadioButtons<string, int>
     {
+        /// <summary>
+        /// Default constructor sets the selected value to int.MaxValue.
+        /// </summary>
         public StringIntRadioButtons()
         {
             this.SelectedValue = int.MaxValue;
         }
 
+        /// <summary>
+        /// Construct radio buttons from an IEnumerable of KeyValue pairs.
+        /// </summary>
+        /// <param name="keysAndValues"></param>
         public StringIntRadioButtons(IEnumerable<KeyValuePair<string, int>> keysAndValues) : base(keysAndValues)
         {
         }
