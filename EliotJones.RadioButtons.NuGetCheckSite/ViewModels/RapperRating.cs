a1d44a8fcd5c87ec94e7d5c7a96c450680b146ec
Eliot Jones <elioty@hotmail.co.uk>
2015-03-22 17:00:20 +0000

    Add missing files

---
+++ EliotJones.RadioButtons.NuGetCheckSite/ViewModels/RapperRating.cs
@@ -0,0 +1,13 @@
+﻿namespace EliotJones.RadioButtons.NuGetCheckSite.ViewModels
+{
+    using EliotJones.RadioButtons.ViewModels;
+    using System.ComponentModel.DataAnnotations;
+
+    public class RapperRating
+    {
+        [Required]
+        public int Rating { get; set; }
+
+        public StringGuidRadioButtons Rappers { get; set; }
+    }
+}
